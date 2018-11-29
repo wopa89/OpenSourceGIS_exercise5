f59fc5f7fc9aacefba3b02e48cdbfcc4129def86
My Name <test@example.de>
2018-11-29 14:32:08 +0100

    join file path with os

--- ex4_import_data.py
+++ ex4_import_data.py
@@ -5,23 +5,24 @@ import grass.script as gscript
 # subprocess is a Python module that lets you execute processes in the command
 # line from within your python script e.g. gdal_transform. 
 import subprocess
+import os 
 
 def main():
 
     # Path to the folder containing all data to be imported
-    path_data = '/Users/chludwig/Data/ex04/'
+    path_data = "C:/Users/nb152/Documents/OpenSourceGIS_exercise_4_data"
 
     # 0. Import cities (survey capture areas)
-    path_cities = path_data + '/nz_cities/nz-survey-capture-areas-sca.shp'
+    path_cities = os.path.join(path_data, 'nz_cities', 'nz-survey-capture-areas-sca.shp')
     gscript.run_command('v.in.ogr', input=path_cities, output='cities')
 
     # 1. Importing railways (nz_railway.shp) ---------------------------------
-    path_railways = path_data + '/nz_railway/nz_railway.shp'
+    path_railways = os.path.join(path_data, 'nz_railway', 'nz_railway.shp')
     gscript.run_command('v.in.ogr', input=path_railways, output='railways')
 
     # 2. Importing rainfall data (avg_annual_rainfall.tif) -------------------
-    path_rainfall = path_data + '/avg_annual_rainfall.tif'
-    path_rainfall_corrected = '/Users/chludwig/Data/ex04/avg_annual_rainfall_corrected.tif'
+    path_rainfall = os.path.join(path_data, 'avg_annual_rainfall.tif')
+    path_rainfall_corrected = os.path.join(path_data, 'avg_annual_rainfall_corrected.tif')
 
     # Use gdal_translate to assign the correct crs to the avg_annual_rainfall.tif
     subprocess.call(['gdal_translate', '-a_srs', 'EPSG:4326', path_rainfall, path_rainfall_corrected])
@@ -30,8 +31,8 @@ def main():
     gscript.run_command('r.import', input=path_rainfall_corrected, output='rainfall')
     
     # 3. Importing sealed roads (nz_road.shp) --------------------------------
-    path_roads = path_data + '/nz_road/nz_road.shp'
-    path_roads_reprojected = '/Users/chludwig/Data/ex04/nz_road/nz_road_reprojected.shp'
+    path_roads = os.path.join(path_data, 'nz_road', 'nz_road.shp')
+    path_roads_reprojected = os.path.join(path_data,  'nz_road','nz_road_reprojected.shp')
 
     # Repoject nz_roads.shp using OGR2OGR
     subprocess.call(['ogr2ogr', '-f', 'ESRI Shapefile', '-t_srs', 'EPSG:32760', path_roads_reprojected, path_roads])
@@ -40,11 +41,11 @@ def main():
     gscript.run_command('v.in.ogr', input=path_roads_reprojected, layer='nz_road_reprojected', output='sealed_roads', where="surface='sealed'")
 
     # 4. Importing airports (nz_airports.csv) --------------------------------
-    path_airports = path_data + '/nz_airport.csv'
+    path_airports = os.path.join(path_data, 'nz_airport.csv')
     gscript.run_command('v.in.ascii', input=path_airports, output='airports', separator='comma', skip=1, x=4, y=5)
 
     # 5. Set region to match rainfall dataset --------------------------------
-    gscript.run_command('g.region', rast='rainfall@PERMANENT')
+    gscript.run_command('g.region', rast='rainfall')
 
     # Print some information about the mapset to check if everything worked 
     # Print projection of mapset 
